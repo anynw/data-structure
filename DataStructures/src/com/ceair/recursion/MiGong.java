187f0eeb57da1ef55c4e05f2e72563ce8178e9b6
wuhuaping <wuhuaping2006@126.com>
2020-03-21 16:29:04 +0800

    调整代码位置

--- DataStructures/src/com/ceair/recursion/MiGong.java
+++ DataStructures/src/com/ceair/recursion/MiGong.java
@@ -1,4 +1,4 @@
-package com.ceair;
+package com.ceair.recursion;
 
 /**
  * 模拟迷宫 8行7列
@@ -25,14 +25,14 @@ public class MiGong {
 
 		// 输出map地图
 		/**
-		 * 	1 1 1 1 1 1 1 
-			1 0 0 0 0 0 1 
-			1 0 0 0 0 0 1 
-			1 0 0 0 0 0 1 
-			1 0 0 0 0 0 1 
-			1 0 0 0 0 0 1 
-			1 0 0 0 0 0 1 
-			1 1 1 1 1 1 1 
+		1 1 1 1 1 1 1 
+		1 0 0 0 0 0 1 
+		1 0 0 0 0 0 1 
+		1 0 0 0 0 0 1 
+		1 0 0 0 0 0 1 
+		1 0 0 0 0 0 1 
+		1 0 0 0 0 0 1 
+		1 1 1 1 1 1 1 
 		 */
 		System.out.println("初始地图:");
 		for (int i = 0; i < 8; i++) {
@@ -41,7 +41,7 @@ public class MiGong {
 			}
 			System.out.println();
 		}
-		
+
 		map[3][1] = 1;
 		map[3][2] = 1;
 		System.out.println("设置挡板过后的地图:");
@@ -51,9 +51,9 @@ public class MiGong {
 			}
 			System.out.println();
 		}
-		
+
 		setWay(map, 1, 1);
-		
+
 		//
 		System.out.println("打印走过的地图:");
 		for (int i = 0; i < 8; i++) {
@@ -62,39 +62,38 @@ public class MiGong {
 			}
 			System.out.println();
 		}
-		
 
 	}
 	// 1.map表示地图
 	// 2.i，j表示从哪里开始走
 	// 3.约定规则：走的方向-》下 右 上 左
 	// 4.0 表示没有走过，可以走；1 表示这是最外层的墙，不通； 2 表示可以到达终点，通； 3 表示 走了一遍，发现不通，需要回溯；
-	
-	public static boolean setWay(int[][] map ,int i ,int j) {
+
+	public static boolean setWay(int[][] map, int i, int j) {
 		// 终点是 map[7][6],如果等于2，表示已经到达终点，返回true
-		if(map[6][5] == 2) {
+		if (map[6][5] == 2) {
 			return true;
 		} else {
 			// 如果此坐标是0，表示可以走，那么假定该位置为2，可以到达终点；
-			if(map[i][j] == 0) {
+			if (map[i][j] == 0) {
 				map[i][j] = 2;
-				if(setWay(map,i+1,j)) {//下
+				if (setWay(map, i + 1, j)) {// 下
 					return true;
-				} else if(setWay(map,i,j+1)) {//右
+				} else if (setWay(map, i, j + 1)) {// 右
 					return true;
-				} else if(setWay(map,i-1,j)) {//上
+				} else if (setWay(map, i - 1, j)) {// 上
 					return true;
-				} else if(setWay(map,i,j-1)) {//左
+				} else if (setWay(map, i, j - 1)) {// 左
 					return true;
 				} else {
 					// 约定的策略都不通，置为3，需要回溯；
 					map[i][j] = 3;
+					return false;
 				}
 			} else { // 不等于0，有可能等于1，2，3.直接返回false；
 				return false;
 			}
 		}
-		return false;
 	}
 
 }
