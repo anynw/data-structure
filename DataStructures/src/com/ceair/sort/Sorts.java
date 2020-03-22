e87f260b867d48bf1c92135cba7724c8e94517f3
wuhuaping <wuhuaping2006@126.com>
2020-03-22 23:16:15 +0800

    插入排序算法

---
+++ DataStructures/src/com/ceair/sort/Sorts.java
@@ -0,0 +1,107 @@
+package com.ceair.sort;
+
+import java.util.Arrays;
+
+/**
+ * 1.排序默认从小到大
+ * 2.模拟10万数据测试排序大概耗时
+ * 3.冒泡排序：15s
+ * 4.选择排序：2s
+ * 5.插入排序：4s
+ * 
+ * @author huaping
+ * @time 2020-03-22 10:54:26 PM
+ */
+public class Sorts {
+
+	public static int DEFAULT_SIZE = 100000;
+	
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 44, -2, 0, 5 };
+		System.out.println("原始数组：" + Arrays.toString(arr));
+		// bubbleSort(arr);
+		// System.out.println("冒泡排序：" + Arrays.toString(arr));
+		// selectSort(arr);
+		// System.out.println("选择排序：" + Arrays.toString(arr));
+		// insertSort(arr);
+		// System.out.println("插入排序：" + Arrays.toString(arr));
+
+		System.out.println("测试各种排序方式耗时：模拟十万数据");
+		int[] array = new int[DEFAULT_SIZE];
+		for (int i = 0; i < DEFAULT_SIZE; i++) {
+			array[i] = (int) (Math.random() * DEFAULT_SIZE);
+		}
+		long start = System.currentTimeMillis();
+//		bubbleSort(array);
+//		selectSort(array);
+		insertSort(array);
+		long end = System.currentTimeMillis();
+		System.out.printf("排序模拟十万数据耗时%d秒", (end - start) / 1000);
+
+	}
+
+	/**
+	 * 冒泡排序
+	 * 
+	 * @param arr
+	 */
+	public static void bubbleSort(int[] arr) {
+		boolean flag = false;
+		int temp = 0;
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					flag = true;
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+			if (!flag) {
+				break;
+			} else {
+				flag = false;
+			}
+		}
+	}
+
+	/**
+	 * 选择排序
+	 * 
+	 * @param arr
+	 */
+	public static void selectSort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			int min = i;
+			int minIndex = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (min > arr[j]) {
+					min = arr[j];
+					minIndex = j;
+				}
+			}
+			if (minIndex != i) {
+				arr[minIndex] = arr[i];
+				arr[i] = min;
+			}
+		}
+	}
+
+	/**
+	 * 插入排序
+	 * 
+	 * @param arr
+	 */
+	public static void insertSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			int insertVal = arr[i];
+			int insertIndex = i - 1;
+			while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+				arr[insertIndex + 1] = arr[insertIndex];
+				insertIndex--;
+			}
+			arr[insertIndex + 1] = insertVal;
+		}
+	}
+
+}
