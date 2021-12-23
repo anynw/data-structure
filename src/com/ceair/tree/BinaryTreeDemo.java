dfe514b15bf3954758d04d5ae24e95c1e8f2c55c
wuhuaping2006@126.com <wuhuaping2006@126.com>
2021-12-23 21:41:32 +0800

    二叉树前序、中序、后序查找

--- src/com/ceair/tree/BinaryTreeDemo.java
+++ src/com/ceair/tree/BinaryTreeDemo.java
@@ -30,6 +30,27 @@ public class BinaryTreeDemo {
         binaryTree.midOrder();
         System.out.println("后序遍历");
         binaryTree.postOrder();
+
+        System.out.println("前序查找");
+        HeroNode heroNode1 = binaryTree.preOrderSearch(1);
+        if (heroNode1 != null) {
+            System.out.println("前序查找已找到:" + heroNode1);
+        } else {
+            System.out.println("前序查找未找到");
+        }
+        HeroNode heroNode2 = binaryTree.midOrderSearch(3);
+        if (heroNode1 != null) {
+            System.out.println("中序查找已找到:" + heroNode2);
+        } else {
+            System.out.println("中序查找未找到");
+        }
+
+        binaryTree.postOrderSearch(1);
+        if (heroNode1 != null) {
+            System.out.println("后序查找已找到:" + heroNode2);
+        } else {
+            System.out.println("后序查找未找到");
+        }
     }
 }
 
@@ -60,6 +81,33 @@ class BinaryTree {
             System.out.println("二叉树为空，不能遍历");
         } else {
             this.root.postOrder();
+        }
+    }
+
+    // 前序查找
+    public HeroNode preOrderSearch(int id) {
+        if (root != null) {
+            return root.preOrderSearch(id);
+        } else {
+            return null;
+        }
+    }
+
+    // 中序查找
+    public HeroNode midOrderSearch(int id) {
+        if (root != null) {
+            return root.midOrderSearch(id);
+        } else {
+            return null;
+        }
+    }
+
+    // 后序查找
+    public HeroNode postOrderSearch(int id) {
+        if (root != null) {
+            return root.postOrderSearch(id);
+        } else {
+            return null;
         }
     }
 
@@ -123,6 +171,82 @@ class HeroNode {
         }
         System.out.println(this);
         // 最后输出父节点
+    }
+
+    /**
+     * 前序查找
+     *
+     * @param id
+     * @return
+     */
+    public HeroNode preOrderSearch(int id) {
+        // 先比较当前节点
+        if (this.id == id) {
+            return this;
+        }
+        // 定义返回值
+        HeroNode resNode = null;
+
+        if (this.left != null) {
+            resNode = this.left.preOrderSearch(id);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+        if (this.right != null) {
+            resNode = this.right.preOrderSearch(id);
+        }
+        return resNode;
+    }
+
+    /**
+     * 中序查找
+     *
+     * @param id
+     * @return
+     */
+    public HeroNode midOrderSearch(int id) {
+        HeroNode resNode = null;
+        if (this.left != null) {
+            resNode = this.left.midOrderSearch(id);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+        if (this.id == id) {
+            return this;
+        }
+        if (this.right != null) {
+            resNode = this.right.midOrderSearch(id);
+        }
+        return resNode;
+    }
+
+    /**
+     * 后序查找
+     *
+     * @param id
+     * @return
+     */
+    public HeroNode postOrderSearch(int id) {
+        HeroNode resNode = null;
+        if (this.left != null) {
+            resNode = this.left.postOrderSearch(id);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+        if (this.right != null) {
+            resNode = this.right.postOrderSearch(id);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+        // 如果左右子树都没有找到，比较当前节点
+        if (this.id == id) {
+            return this;
+        }
+        return resNode;
     }
 
 
