c41f0ba2e47e197df01981b9c7f5f2977b4a3adc
wuhuaping2006@126.com <wuhuaping2006@126.com>
2022-01-06 01:37:29 +0800

    马踏棋盘-贪心算法-最优处理

--- src/com/ceair/horse/HorseChessboard.java
+++ src/com/ceair/horse/HorseChessboard.java
@@ -2,6 +2,7 @@ package com.ceair.horse;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
  * 马踏棋盘问题
@@ -22,13 +23,14 @@ public class HorseChessboard {
         int column = 1;
         int[][] chessboard = new int[X][Y];
         visited = new boolean[X * Y];
+        System.out.println("计算开始：");
         long start = System.currentTimeMillis();
         traversalChessboard(chessboard, row - 1, column - 1, 1);
         long end = System.currentTimeMillis();
-        System.out.println("耗时："+(end-start)/1000 + "s");
+        System.out.println("耗时：" + (end - start) + "ms");
 
-        for (int[] rows:chessboard){
-            for (int step : rows){
+        for (int[] rows : chessboard) {
+            for (int step : rows) {
                 System.out.print(step + "\t");
             }
             System.out.println();
@@ -58,6 +60,8 @@ public class HorseChessboard {
         chessboard[row][column] = step;
         visited[row * X + column] = true;
         ArrayList<Point> ps = next(new Point(column, row));
+        // 排序
+        sort(ps);
         while (!ps.isEmpty()) {
             Point p = ps.remove(0);
             if (!visited[p.y * X + p.x]) {
@@ -120,7 +124,28 @@ public class HorseChessboard {
         if ((p8.x = curPoint.x - 2) >= 0 && (p8.y = curPoint.y + 1) < Y) {
             list.add(p8);
         }
-
         return list;
+    }
+
+    /**
+     * 非递减排序，减少回溯的次数，性能提高，1400ms->12ms
+     *
+     * @param ps
+     */
+    public static void sort(ArrayList<Point> ps) {
+        ps.sort(new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                int count1 = next(o1).size();
+                int count2 = next(o2).size();
+                if (count1 > count2) {
+                    return 1;
+                } else if (count1 == count2) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+            }
+        });
     }
 }
